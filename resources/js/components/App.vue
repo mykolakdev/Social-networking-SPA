d18d75994b584a4441b0356526a3f905dcc9a5ea
mykolakdev <kriuchkovmykola@hotmail.com>
2020-02-13 21:05:51 +0100

    vue router installed and home configuration

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+     <h1>APP</h1>
+     <router-view/>
+ </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
