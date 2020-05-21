1eda150bffbd73311768cf5b9114abc895ea1719
mykolakdev <kriuchkovmykola@hotmail.com>
2020-05-21 16:00:00 +0200

    Routes index UserPost and refactor api routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import VueRouter from  'vue-router';
-import Start from "./views/Start";
 import NewsFeed from "./views/NewsFeed";
-
+import UserShow from "./views/Users/Show";
 Vue.use(VueRouter);
 
 export default new VueRouter({
@@ -13,6 +12,11 @@ export default new VueRouter({
             path: '/',
             name: 'home',
             component: NewsFeed,
+        },
+        {
+            path: '/users/:userId',
+            name: 'user.show',
+            component: UserShow,
         }
     ]
 });
